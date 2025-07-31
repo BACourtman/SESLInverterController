c2bf72e6c316ebf1a536ee37bff8d266e21ddcde
BACourtman <bacourtman@gmail.com>
2025-07-31 10:41:56 -0400

    ADC changes to fix bad voltage divider

--- Helpers/adc_monitor.c
+++ Helpers/adc_monitor.c
@@ -9,7 +9,7 @@
 static const float R1 = 2800 ; // Resistor 1 value in ohms
 static const float R2 = 5100 ; // Resistor 2 value in ohms
 static const float v_per_a[3] = {2.5e-3, 2.5e-3, 1.25e-4};     // V/A for each channel
-static const float offset_v[3] = {5.0*R1/(R1+R2)/2, 5.0*R1/(R1+R2)/2, 5.0*R1/(R1+R2)/2};    // Offset voltage for each channel
+static const float offset_v[3] = {2.5*R1/(R1+R2), 2.5*R1/(R1+R2), 2.5*R1/(R1+R2)};    // Offset voltage for each channel
 
 void adc_monitor_init(void) {
     adc_init();
--- Helpers/adc_monitor.h
+++ Helpers/adc_monitor.h
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 
 #define ADC_DISCONNECT_THRESHOLD 10
-#define MAX_DC_CURRENT 20.0f
+#define MAX_DC_CURRENT 5000.0f
 #define MAX_RMF_CURRENT 400.0f
 
 void adc_monitor_init(void);
